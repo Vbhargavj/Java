673505baf587532a10b916a29e1cd281db21e78b
bhargav <bhargavvadukar3@gmail.com>
2023-01-01 23:01:15 +0530

    java

---
+++ vbj_15_class.java
@@ -0,0 +1,24 @@
+class greeting {
+    int salary;
+    String s;
+
+    public void fi() {
+        System.out.println("Helllo this is my first class");
+    }
+
+    public void getData() {
+        System.out.println("tusi " + salary);
+        System.out.println("some massage+++++++++ " + s);
+    }
+}
+
+public class vbj_15_class {
+    public static void main(String[] args) {
+        System.out.println("Welcome to most popular language ");
+        greeting g = new greeting();
+        g.salary = 10;
+        g.s = "this is bhargav";
+        g.fi();
+        g.getData();
+    }
+}
---
+++ vbj_ex_on_modifier.java
@@ -0,0 +1,39 @@
+
+
+class surface {
+    private int radius, height;
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+// using constructor 
+    public surface(int rad, int heg) {
+        this.height = heg;
+        this.radius = rad;
+    }
+    
+}
+
+public class vbj_ex_on_modifier {
+    public static void main(String[] args) {
+        // surface s = new surface();
+        // s.setHeight(34);
+        // s.setRadius(12);
+        // System.out.println( s.getRadius());
+        // System.out.println( 2*3.14*s.getHeight());
+        surface s = new surface(2, 1);
+        System.out.println( s.getRadius());        
+    }
+}
